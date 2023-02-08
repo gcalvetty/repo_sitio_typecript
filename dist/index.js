1dedd701fae4ab030137d68a79f5d64fc24d92e1
Gcalvetty <gcalvetty@gmail.com>
2023-02-08 13:03:22 -0400

    Creando el Depurador

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,7 @@
 "use strict";
 let var1 = "Hola Mundo";
 var1 = "Desarrollo Web";
+var1 = "Nos vemos";
+console.log(var1);
+console.log(typeof []);
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 let var1 : string = "Hola Mundo"
 var1 = "Desarrollo Web"
+
+var1 = "Nos vemos"
+
+console.log(var1)
+console.log(typeof [])
