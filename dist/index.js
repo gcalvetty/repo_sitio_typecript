0773fe4f6b5bea7fdfcf3a666f62cc2c15016808
Gcalvetty <gcalvetty@gmail.com>
2023-02-08 14:10:14 -0400

    Aprendiendo los Tipos de TS

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,45 @@ var1 = "Desarrollo Web";
 var1 = "Nos vemos";
 console.log(var1);
 console.log(typeof []);
+let extincionDinosarurios = 76000000;
+let dinosaurioFavorito = "T-rex";
+let extintos = true;
+let mivariable = "este es mi valor";
+let mivariable2;
+mivariable = "Hola Dev";
+mivariable2 = 42;
+function gecndev(config) {
+    return config;
+}
+let animales = ['perro', 'gato', 'pollo'];
+let animales2 = ['perro', 'gato', 'pollo'];
+let nums = [1, 2, 3];
+let nums2 = [];
+let checks = [];
+let nums3 = [];
+let tupla = [1, 'Desarrollo Web'];
+let tupla2 = [1, ['Desarrollo Web', 'Hola']];
+var Talla;
+(function (Talla) {
+    Talla["Chica"] = "s";
+    Talla["Mediana"] = "m";
+    Talla["Grande"] = "l";
+    Talla["ExtraGrande"] = "xl";
+})(Talla || (Talla = {}));
+console.log(Talla.Chica);
+const estado = 2;
+const objeto = { id: 1, nombre: 'Guillermo' };
+const objeto2 = { id: 2 };
+objeto.id = 47;
+const objeto3 = { id: 3 };
+const objeto4 = {
+    id: 4,
+    nombre: 'GECNDEV',
+    talla: Talla.Grande
+};
+const objeto5 = {
+    id: 5,
+    nombre: 'Guillermo',
+    talla: Talla.ExtraGrande
+};
 //# sourceMappingURL=index.js.map
