c5adfe54e8457431f330a6b08ca27c9cc31bf83f
Gcalvetty <gcalvetty@gmail.com>
2023-02-17 21:31:11 -0400

    "Configurando el entorno"

--- dist/index.js
+++ dist/index.js
@@ -1,48 +1,15 @@
 "use strict";
-let var1 = "Hola Mundo";
-var1 = "Desarrollo Web";
-var1 = "Nos vemos";
-console.log(var1);
-console.log(typeof []);
-let extincionDinosarurios = 76000000;
-let dinosaurioFavorito = "T-rex";
-let extintos = true;
-let mivariable = "este es mi valor";
-let mivariable2;
-mivariable = "Hola Dev";
-mivariable2 = 42;
-function gecndev(config) {
-    return config;
-}
-let animales = ['perro', 'gato', 'pollo'];
-let animales2 = ['perro', 'gato', 'pollo'];
-let nums = [1, 2, 3];
-let nums2 = [];
-let checks = [];
-let nums3 = [];
-let tupla = [1, 'Desarrollo Web'];
-let tupla2 = [1, ['Desarrollo Web', 'Hola']];
-var Talla;
-(function (Talla) {
-    Talla["Chica"] = "s";
-    Talla["Mediana"] = "m";
-    Talla["Grande"] = "l";
-    Talla["ExtraGrande"] = "xl";
-})(Talla || (Talla = {}));
-console.log(Talla.Chica);
-const estado = 2;
-const objeto = { id: 1, nombre: 'Guillermo' };
-const objeto2 = { id: 2 };
-objeto.id = 47;
-const objeto3 = { id: 3 };
-const objeto4 = {
-    id: 4,
-    nombre: 'GECNDEV',
-    talla: Talla.Grande
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-const objeto5 = {
-    id: 5,
-    nombre: 'Guillermo',
-    talla: Talla.ExtraGrande
-};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('Esta corriento el FrameWork Express');
+});
+app.listen(port, () => {
+    return console.log(`server is listening on ${port}`);
+});
 //# sourceMappingURL=index.js.map
